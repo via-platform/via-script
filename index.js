513c59e24e1b8d25e0a38192feb88d3bfe7ff8de
Dakota Gordon <dakota@via.world>
2018-11-16 17:02:21 -0600

    Export Series.

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const math = require('./src/math');
 const properties = require('./src/properties');
 const time = require('./src/time');
 const ta = require('./src/ta');
+const Series = require('./src/series');
 
 module.exports = {
     ...core,
     ...math,
     ...properties,
     ...time,
-    ...ta
+    ...ta,
+    Series
 };
